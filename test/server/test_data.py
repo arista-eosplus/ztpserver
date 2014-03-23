3d0a709b42e39ba644e4cd87529c43346efe8cb6
Peter Sprygada <sprygada@aristanetworks.com>
2014-03-23 09:09:20 -0400

    fixed test cases

--- test/server/test_data.py
+++ test/server/test_data.py
@@ -61,50 +61,11 @@ class Functions(unittest.TestCase):
     def test_functions_regex_false(self):
         self.assertFalse(self.Functions.regex("\d+", "test"))
 
-class TestInterface(unittest.TestCase):
-
-    def test_create_interface(self):
-        obj = ztpserver.data.Interface('Ethernet')
-        self.assertEqual(repr(obj), "Interface(name=Ethernet, neighbors=0)")
-
-    def test_add_neighbor(self):
-        obj = ztpserver.data.Interface('Ethernet')
-        obj.add_neighbor('test_device', 'test_port')
-        self.assertEqual(repr(obj), "Interface(name=Ethernet, neighbors=1)")
-
-        neighbor = obj.neighbors[0]
-        self.assertEqual(neighbor.device, 'test_device')
-        self.assertEqual(neighbor.port, 'test_port')
-
-class TestInterfaces(unittest.TestCase):
-
-    def test_create_interfaces(self):
-        obj = ztpserver.data.Interfaces()
-        self.assertEqual(repr(obj), "Interfaces(count=0)")
-
-    def test_add_interface_from_string(self):
-        obj = ztpserver.data.Interfaces()
-        obj.add_interface("Ethernet")
-        self.assertEqual(repr(obj['Ethernet']),
-                         "Interface(name=Ethernet, neighbors=0)")
-
-    def test_add_interface_from_object_valid(self):
-        intf = ztpserver.data.Interface('Ethernet')
-        obj = ztpserver.data.Interfaces()
-        obj.add_interface(intf)
-        self.assertEqual(repr(obj['Ethernet']),
-                         "Interface(name=Ethernet, neighbors=0)")
-
-    def test_add_interface_from_object_invalid(self):
-        intf = dict(name='Ethernet')
-        obj = ztpserver.data.Interfaces()
-        self.assertRaises(TypeError, obj.add_interface, intf)
-
 class TestNode(unittest.TestCase):
 
     def test_node_creation(self):
         obj = ztpserver.data.Node()
-        self.assertEqual(repr(obj), "Node(interfaces=0)")
+        self.assertEqual(repr(obj), "Node(neighbors=0)")
 
     def test_node_creation_with_kwargs(self):
         kwargs = dict(model='vEOS',
@@ -113,21 +74,17 @@ class TestNode(unittest.TestCase):
                       version='4.12.0')
         obj = ztpserver.data.Node(**kwargs)
 
-        self.assertEqual(repr(obj), "Node(interfaces=0)")
+        self.assertEqual(repr(obj), "Node(neighbors=0)")
         self.assertEqual(obj.model, 'vEOS')
         self.assertEqual(obj.systemmac, '00:1c:73:aa:11:bb')
         self.assertEqual(obj.serialnumber, '0123456789')
         self.assertEqual(obj.version, '4.12.0')
 
-    def test_node_add_interface_valid(self):
+    def test_node_add_neighbors_valid(self):
         obj = ztpserver.data.Node()
-        obj.add_interface('Ethernet')
-        self.assertEqual(repr(obj), "Node(interfaces=1)")
-        self.assertIsNotNone(obj.interfaces('Ethernet'))
-
-    def test_node_add_interface_invalid(self):
-        obj = ztpserver.data.Node()
-        self.assertRaises(TypeError, obj.add_interface, 'test')
+        obj.add_neighbors({"Ethernet": [{"device": "test", "port": "test"}]})
+        self.assertEqual(repr(obj), "Node(neighbors=1)")
+        self.assertIsNotNone(obj.neighbors('Ethernet'))
 
 class TestNeighborDb(unittest.TestCase):
 
@@ -139,32 +96,11 @@ class TestNeighborDb(unittest.TestCase):
         fn = os.path.join(os.getcwd(), "test/data/neighbordb.yml")
         obj = ztpserver.data.NeighborDb()
         obj.load(fn)
-        self.assertEqual(repr(obj), "NeighborDb(globals=3, nodes=2)")
+        self.assertEqual(repr(obj), "NeighborDb(globals=4, nodes=2)")
 
     def test_load_invalid_filename(self):
         obj = ztpserver.data.NeighborDb()
         self.assertRaises(IOError, obj.load, '/tmp/fake/file')
-
-    def test_parse_interface_values(self):
-        obj = ztpserver.data.NeighborDb()
-        values = dict(device='device', port='port', tags='tags')
-        result = obj._parse_interface('Ethernet', values)
-        expected = ('Ethernet', 'device', 'port', 'tags')
-        self.assertEqual(result, expected)
-
-    def test_parse_interface_any(self):
-        obj = ztpserver.data.NeighborDb()
-        result = obj._parse_interface('Ethernet', 'any')
-        expected = ('Ethernet', 'any', 'any', None)
-        self.assertEqual(result, expected)
-
-    def test_parse_interface_none(self):
-        obj = ztpserver.data.NeighborDb()
-        result = obj._parse_interface('Ethernet', 'none')
-        expected = ('Ethernet', None, None, None)
-        self.assertEqual(result, expected)
-
-
 
 class TestPattern(unittest.TestCase):
 
@@ -175,15 +111,15 @@ class TestPattern(unittest.TestCase):
 
     def test_create_pattern_with_kwargs(self):
         kwargs = dict(node='abc123',
-                      variables={ 'var': 'test'},
-                      interfaces={ 'Ethernet1': 'any' })
+                      variables={'var': 'test'},
+                      interfaces=[{'Ethernet1': 'any'}])
 
         obj = ztpserver.data.Pattern('test', 'test', **kwargs)
         self.assertEqual(obj.name, 'test')
         self.assertEqual(obj.definition, 'test')
         self.assertEqual(obj.node, 'abc123')
         self.assertDictEqual({'var': 'test'}, obj.variables)
-        self.assertDictEqual({'Ethernet1': 'any'}, obj.interfaces)
+        self.assertEqual(1, len(obj.interfaces))
 
     def test_add_interface(self):
         obj = ztpserver.data.Pattern('test', 'test')
