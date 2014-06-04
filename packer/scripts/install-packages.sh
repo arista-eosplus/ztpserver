df4c821a09089fb4cfeacfa431c4882631de8f19
phil-arista <phil@arista.com>
2014-06-04 09:40:39 -0400

    optimized installation

--- packer/scripts/install-packages.sh
+++ packer/scripts/install-packages.sh
@@ -3,13 +3,7 @@
 # Remove unnecessary packages
 yum -y remove kbd
 
-#Since we removed NetworkManager, let's setup the network service
-systemctl stop NetworkManager.service
-systemctl disable NetworkManager.service
-systemctl restart network.service
-systemctl enable network.service
 yum -y remove NetworkManager
-
 yum -y remove plymouth
 yum -y remove uboot-tools
 
@@ -20,9 +14,13 @@ yum --assumeyes clean all
 
 #Install ztps-related related packages
 yum -y install gcc make gcc-c++
-yum -y install pip
 yum -y install git
 yum -y install net-tools
 yum -y install dhcp
 yum -y install bind
 yum -y install ejabberd
+
+#systemctl stop NetworkManager.service
+#systemctl disable NetworkManager.service
+systemctl start network.service
+systemctl enable network.service
