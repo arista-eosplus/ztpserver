c06e1050f4b5904bb1529c9ee6a58931d3513455
phil-arista <phil@arista.com>
2014-06-04 09:24:33 -0400

    optimized installation

--- packer/scripts/install-packages.sh
+++ packer/scripts/install-packages.sh
@@ -1,5 +1,23 @@
 #!/bin/sh -x
 
+# Remove unnecessary packages
+yum -y remove kbd
+
+#Since we removed NetworkManager, let's setup the network service
+systemctl stop NetworkManager.service
+systemctl disable NetworkManager.service
+systemctl restart network.service
+systemctl enable network.service
+yum -y remove NetworkManager
+
+yum -y remove plymouth
+yum -y remove uboot-tools
+
+#Grab any updates and cleanup
+yum --assumeyes update yum
+yum --assumeyes update
+yum --assumeyes clean all
+
 #Install ztps-related related packages
 yum -y install gcc make gcc-c++
 yum -y install pip
