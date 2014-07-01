8f79ac476bdce16ac762faa0aa11ab6da2388b50
phil-arista <phil@arista.com>
2014-07-01 14:00:07 -0400

    adding script to set up vmnets automatically

---
+++ packer/vEOS/VMware/setup-fusion.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Assume that we are using Fusion
+cd /Applications/VMware\ Fusion.app/Contents/Library/
+
+#Check the current status
+printf "Starting the VMware network setup....\n\nCurrent VMware Fusion current network setup\n-------------------------------------\n"
+./vmnet-cli --status
+
+#Create an array with the vmnets we want to create/modify
+declare -a VMNETS
+VMNETS=(2 3 4 5 6 7 9 10 11)
+
+for i in "${VMNETS[@]}"; do
+  printf "Creating/Modifying vmnet$i\n"
+  NET=$(($i+128))
+  ./vmnet-cfgcli vnetcfgadd VNET_$i_DHCP no
+  ./vmnet-cfgcli vnetcfgadd VNET_$i_HOSTONLY_SUBNET 172.16.$NET.0
+  ./vmnet-cfgcli vnetcfgadd VNET_$i_HOSTONLY_NETMASK 255.255.255.0
+  ./vmnet-cfgcli vnetcfgadd VNET_$i_VIRTUAL_ADAPTER yes
+done
+
+./vmnet-cli --configure
+./vmnet-cli --stop
+./vmnet-cli --start
+sleep 2
+./vmnet-cli --status
