98976926e7c84c805c3362f37f5cf1f17bab6f91
Jere Julian <jere@arista.com>
2014-08-27 20:53:54 -0400

    Update version strings to 1.2.0-devel

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
 # CHANGELOG
 
 ## 1.0
+
+## 1.1.0
+
+See http://ztpserver.readthedocs.org/en/v1.1.0/ReleaseNotes1.1.html
+
--- ztpserver/__init__.py
+++ ztpserver/__init__.py
@@ -30,6 +30,6 @@
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 #
-__version__ = '1.1.0'
+__version__ = '1.2.0-dev'
 __author__ = 'Arista Networks'
 
